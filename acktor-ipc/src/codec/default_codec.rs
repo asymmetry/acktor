8e5796752ad6e67e86b18baa4c6c235dcb51574e
Chao Gu <guchao.pku@gmail.com>
2026-04-23 17:41:58 -0700

    cargo fmt

--- acktor-ipc/src/codec/default_codec.rs
+++ acktor-ipc/src/codec/default_codec.rs
@@ -343,8 +343,25 @@ impl_encode_decode_for_tuple!(T0<0>, T1<1>, T2<2>, T3<3>, T4<4>, T5<5>);
 impl_encode_decode_for_tuple!(T0<0>, T1<1>, T2<2>, T3<3>, T4<4>, T5<5>, T6<6>);
 impl_encode_decode_for_tuple!(T0<0>, T1<1>, T2<2>, T3<3>, T4<4>, T5<5>, T6<6>, T7<7>);
 impl_encode_decode_for_tuple!(
-    T0<0>, T1<1>, T2<2>, T3<3>, T4<4>, T5<5>, T6<6>, T7<7>, T8<8>
+    T0<0>,
+    T1<1>,
+    T2<2>,
+    T3<3>,
+    T4<4>,
+    T5<5>,
+    T6<6>,
+    T7<7>,
+    T8<8>
 );
 impl_encode_decode_for_tuple!(
-    T0<0>, T1<1>, T2<2>, T3<3>, T4<4>, T5<5>, T6<6>, T7<7>, T8<8>, T9<9>
+    T0<0>,
+    T1<1>,
+    T2<2>,
+    T3<3>,
+    T4<4>,
+    T5<5>,
+    T6<6>,
+    T7<7>,
+    T8<8>,
+    T9<9>
 );
